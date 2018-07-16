3016aecfc95844457ceb12beb4fd99e257d2396f
Luiz de Milon <luiz.de.milon@gmail.com>
2018-07-16 08:44:36 -0400

    add GetListenHistory

--- fetch.go
+++ fetch.go
@@ -1,1 +1,9 @@
 package listenbrainz
+
+import "net/http"
+
+func GetListenHistory(user string, count int) (*http.Response, error) {
+	url := "https://api.listenbrainz.org/1/user/" + user + "/listens"
+
+	return http.Get(url)
+}
