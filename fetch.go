42996d006152cdd843ba19736716cfd691f266ea
Luiz de Milon <luiz.de.milon@gmail.com>
2020-01-24 17:03:27 -0400

    lint

--- fetch.go
+++ fetch.go
@@ -2,7 +2,8 @@ package listenbrainz
 
 import "net/http"
 
-func GetListenHistory(user string, count int) (*http.Response, error) {
+// GetListenHistory fetches the listen history of the given user.
+func GetListenHistory(user string) (*http.Response, error) {
 	url := "https://api.listenbrainz.org/1/user/" + user + "/listens"
 
 	return http.Get(url)
